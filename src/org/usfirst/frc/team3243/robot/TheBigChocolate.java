9022f7d19c1a4b86828e6059222763b0d0ba349d
FLL <FLL@LAPTOP-B.Home>
2018-07-20 17:44:40 -0600

    Eat it or beat it.

---
+++ src/org/usfirst/frc/team3243/robot/TheBigChocolate.java
@@ -0,0 +1,10 @@
+package src.org.usfirst.frc.team3243.robot;
+
+public class TheBigChocolate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static final int a = 10;
+	}
+
+}
