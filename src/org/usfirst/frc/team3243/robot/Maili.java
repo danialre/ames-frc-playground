f0f3a5fe84fa04ac20df485b57a9f465ab92c4f2
vumai <vumai@DESKTOP-P4ND4.Home>
2018-07-20 18:25:55 -0600

    Done.

--- src/org/usfirst/frc/team3243/robot/Maili.java
+++ src/org/usfirst/frc/team3243/robot/Maili.java
@@ -3,4 +3,5 @@ package src.org.usfirst.frc.team3243.robot;
 public class Maili {
 	public static final int day = 20;
 	String hi = "What's up everyone? What name are you planning to name the upcoming FRC competition? Kepler 10b?";
+	protected int y = 32;
 }
